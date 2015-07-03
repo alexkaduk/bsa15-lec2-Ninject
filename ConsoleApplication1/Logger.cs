a0763c4fc9013dfa489425b093447f058822f937
alexkaduk <o.kaduk@gmail.com>
2015-07-03 22:07:17 +0300

    1: Реалізовано Ninject (класи без функціоналу)

---
+++ ConsoleApplication1/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Logger : ILogger
+    {
+        public void Log(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("result true");
+            }
+            else
+            {
+                Console.WriteLine("result fasle");
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            iocInit();
+
+            var checker = Ioc.Get<IChecker>();
+
+            var result = checker.Check();
+
+            var logger = Ioc.Get<ILogger>();
+
+            logger.Log(result);
+
+        }
+
+        private static void iocInit()
+        {
+            var simple = true;
+
+            Ioc.Init((kernel) =>
+            {
+                if (simple)
+                {
+                    kernel.Bind<IChecker>().To<Checker>().InTransientScope();
+                    kernel.Bind<ILogger>().To<Logger>().InTransientScope(); 
+                    // kernel.Bind<ICalculator>().To<SimpleCalculator>().InSingletonScope();
+                    // kernel.Bind<ICalculator>().To<SimpleCalculator>().InThreadScope(); // 1 instance pre thread
+                }
+                else
+                {
+                    kernel.Bind<IChecker>().To<Checker>().InTransientScope();
+                    kernel.Bind<ILogger>().To<Logger>().InTransientScope(); 
+                }
+                //kernel.Bind<ServerProxy>().ToSelf();
+            });
+        }
+    }
+}
